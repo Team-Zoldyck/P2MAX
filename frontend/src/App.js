70df4c50904ec351eeac12f92064dc3c30414a16
sticklo <mafimisebiadebayo@gmail.com>
2022-01-27 12:47:41 +0100

    project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline flex text-center justify-center">
+    Hello world!
+  </h1>
+  );
+}
+
+export default App;
