017223afdac48acd5ce25e39f4478e3e37a946d9
Sticklo <mafimisebiadebayo@gmail.com>
2022-02-05 20:18:47 +0100

    hero implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <div>
-         <Navbar />
+         <LandingPage />
     </div>
   );
 }
