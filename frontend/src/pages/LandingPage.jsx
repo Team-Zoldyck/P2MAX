6358c416984c60f10b50aa9fcb10c7869ae520b1
Mary <marieshix98@gmail.com>
2022-05-30 21:19:31 +0300

    Update routes

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+
 import rafiki_hero from "../assets/images/rafiki_hero.png";
-import hero_btn_icon from "../assets/images/hero_btn_icon.png"
+import hero_btn_icon from "../assets/images/hero_btn_icon.png";
 
 const LandingPage = () => {
   return (
     <div>
-      <Navbar />
       <div className="flex flex-col md:flex-row  gap-6 py-20 justify-center items-center">
         <div>
           <h1 className="text-3xl md:text-5xl font-bold">
@@ -22,7 +21,11 @@ const LandingPage = () => {
             Experience money transfer in a whole new way with P2Max
           </p>
           <div className="mt-6 pb-6 text-center md:text-left md:pb-0">
-            <button className="flex justify-center items-center gap-4" id="btn">  <img src={hero_btn_icon} alt="hero_btn_icon" />SEND MONEY NOW</button>
+            <button className="flex justify-center items-center gap-4" id="btn">
+              {" "}
+              <img src={hero_btn_icon} alt="hero_btn_icon" />
+              SEND MONEY NOW
+            </button>
           </div>
         </div>
 
--- frontend/src/router/RoutesComponent.js
+++ frontend/src/router/RoutesComponent.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import Login from "../authentication/Login";
 import LandingPage from "../pages/LandingPage";
+import Transaction from "../pages/transfer/Transaction";
+import Support from "../pages/Support";
+import SendMoney from "../pages/transfer/sendmoney/SendMoney";
+import RequestMoney from "../pages/transfer/requestmoney/RequestMoney";
 
 function RoutesComponent() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="home" element={<LandingPage />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/transfers" element={<Transaction />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/sendmoney" element={<SendMoney />} />
+        <Route path="/requestmoney" element={<RequestMoney />} />
       </Routes>
     </>
   );
