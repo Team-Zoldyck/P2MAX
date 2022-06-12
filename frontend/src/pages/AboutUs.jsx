adc579c066ff6ee67220b03c021f4c172dd3a063
Godson <imiebo.godson@gmail.com>
2022-06-12 09:39:04 +0100

    added initial files for about us page

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+
+
+const AboutUs = () => {
+
+  return (
+
+    <div className="h-screen flex justify-center items-center">
+        <p>COMING SOON!</p>
+    </div>
+  )
+}
+
+export default AboutUs
