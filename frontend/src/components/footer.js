b3a4f473a0c92b425f1a98b59214671360be33bb
Godson <imiebo.godson@gmail.com>
2022-06-19 14:43:48 +0100

    Fix: fixed the footer layout on medium and smaller screens

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,22 +1,22 @@
 const Footer = () => {
     return (
-        <div className="p-12 py-20 bg-secondary text-white flex items-center justify-between">
-            <div className="h-[10rem]">
+        <div className="h-fit p-12 py-20 bg-secondary text-white flex flex-col sm:flex-row items-start justify-between">
+            <div className="h-fit mb-[50px]">
                 <h5 className="font-bold text-[1.5rem] mb-5">P2Max</h5>
                 <span>1/3 Pound Road, Aba</span>
             </div>
 
-            <div className="h-[10rem]">
+            <div className="h-fit mb-[50px]">
                 <h5 className="font-bold text-[1.5rem] mb-5">Company</h5>
 
-                    <a className="block mb-3" href="/">About Us</a>                
-                    <a className="block mb-3" href="/">Privacy</a>                
-                    <a className="block mb-3" href="/">Terms</a>                
-                    <a className="block mb-3" href="/">Legal</a>   
+                <a className="block mb-3" href="/">About Us</a>                
+                <a className="block mb-3" href="/">Privacy</a>                
+                <a className="block mb-3" href="/">Terms</a>                
+                <a className="block mb-3" href="/">Legal</a>   
 
             </div>
 
-            <div className="h-[10rem]">
+            <div className="h-fit mb-[50px]">
                 <h5 className="font-bold text-[1.5rem] mb-5">Contact</h5>
 
                     <span className="block mb-3">info@worldcitizen.com</span>
