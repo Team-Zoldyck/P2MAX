8758f0614d57b7bd6019166c442ce00dcdcd3b5a
code-art4 <ojotriumph4@gmail.com>
2022-06-21 19:09:56 +0100

    added responsiveness to the FAQs

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,12 +1,12 @@
 const Footer = () => {
     return (
-        <div className="mt-20 p-12 py-20 bg-secondary text-white flex items-center justify-between">
-            <div className="h-[10rem]">
+        <div className="mt-20 sm:p-12 py-20 bg-secondary text-white flex sm:items-center justify-between flex-col sm:flex-row px-8 gap-y-8">
+            <div className="sm:h-[10rem]">
                 <h5 className="font-bold text-[1.5rem] mb-5">P2Max</h5>
                 <span>1/3 Pound Road, Aba</span>
             </div>
 
-            <div className="h-[10rem]">
+            <div className="sm:h-[10rem]">
                 <h5 className="font-bold text-[1.5rem] mb-5">Company</h5>
 
                     <a className="block mb-3" href="/">About Us</a>                
@@ -16,7 +16,7 @@ const Footer = () => {
 
             </div>
 
-            <div className="h-[10rem]">
+            <div className="sm:h-[10rem]">
                 <h5 className="font-bold text-[1.5rem] mb-5">Contact</h5>
 
                     <span className="block mb-3">info@worldcitizen.com</span>
