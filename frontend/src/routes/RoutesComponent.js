11e3b64ef7f4bc4703abf7ad983d533de7a2bc99
Mary <marieshix98@gmail.com>
2022-06-28 11:30:40 +0300

    Remove duplicate navbar component

--- frontend/src/routes/RoutesComponent.js
+++ frontend/src/routes/RoutesComponent.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import LandingPage from "../pages/LandingPage";
 import Transaction from "../pages/transfer/Transaction";
 import Support from "../pages/Support";
 import SendMoney from "../pages/transfer/sendmoney/SendMoney";
 import RequestMoney from "../pages/transfer/requestmoney/RequestMoney";
-import FAQs from "../pages/FAQs";
-import About from "../pages/About";
+import FAQsCard from "../components/FAQs";
+import AboutUs from "../pages/AboutUs";
 import Login from "../pages/Login";
 
 function RoutesComponent() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<AboutUs />} />
         <Route path="/transfers" element={<Transaction />} />
         <Route path="/support" element={<Support />} />
-        <Route path="/faqs" element={<FAQs />} />
+        <Route path="/faqs" element={<FAQsCard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sendmoney" element={<SendMoney />} />
         <Route path="/requestmoney" element={<RequestMoney />} />
