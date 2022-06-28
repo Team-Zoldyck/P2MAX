204d71c56420ab8cca027b3f64935ec40f7097dc
Olspe <mo.amin6@gmail.com>
2022-06-27 19:21:27 -0500

    attempt 2: signup_plus_onemorestep

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook, FaApple} from 'react-icons/fa'
-const Login =()=>{
+const SignUp =()=>{
 return(
     <>
         <Navbar />
@@ -10,12 +10,13 @@ return(
             <div  className="container mx-auto lg:my-14 flex lg:flex-row flex-col justify-between">
                 <div className="my-24 ">
                     <div className="lg:p-24 pl-4">
-                        <h2 className="mb-4 font-bold text-3xl text-secondary">Sign In</h2>
-                        <p className="text-xl text-blue-900 font-bold">Hey there. Get right back into your account</p>
+                        <h2 className="mb-4 font-bold text-3xl text-secondary">Sign Up</h2>
+                        <p className="text-xl  text-blue-900 font-bold">Hey there. There is a lot you are missing out on.
+                            <br /> Having an account with us fixes that.</p>
                         <p className="my-12"> 
-                        Yet to have an account?
+                            Already have an account?
                         <span className=" ml-2"> 
-                        <a href="signup" className="font-bold text-secondary hover:underline decoration-2">Create one!</a>
+                        <a href="login" className="font-bold text-secondary hover:underline decoration-2">Sign in</a>
                         </span>
                         </p>
                     </div>
@@ -23,20 +24,19 @@ return(
                 <div className="bg-neutral-100 lg:w-1/3">
                     <div className="px-6 py-2 mx-auto my-3">
                         <form action="index.html" method="post">
-                            <label htmlFor="name" className="block mb-2 text-secondary font-semibold">Email address or username</label>
-                            <input type="text" className="w-11/12 h-10 rounded bg-white-600 border-2 border-blue-200 hover:shadow-md hover:shadow-blue-600" name="username"></input>
+                            <label htmlFor="name" className="block mb-2 text-secondary font-semibold">Full name</label>
+                            <input type="text" className="w-11/12 h-10 rounded bg-white-600 border-2 border-blue-200 hover:shadow-md hover:shadow-blue-600" name="name"></input>
+                            <label htmlFor="email" className="block mt-6 mb-2 text-secondary font-semibold">Email address</label>
+                            <input type="email" className="w-11/12 h-10 rounded bg-white-600 border-2 border-blue-200 hover:shadow-md hover:shadow-blue-600" name="email"></input>
                             <label htmlFor="password" className="block mt-6 mb-2 text-secondary font-semibold">Password</label>
                             <input type="text" className="w-11/12 h-10 rounded bg-white-600 border-2 border-blue-200 hover:shadow-md hover:shadow-blue-600" name="password"></input><br/>
-                            <div className="flex justify-end pr-8 mb-8 ">
-                                <a href="#" className="text-secondary hover:underline decoration-2">Forgot your password?</a>
-
-                            </div>
-                            <div className="flex justify-center  mb-8 ">
-                                <button type="submit" className="py-2 rounded bg-secondary text-white w-10/12 lg:w-full">LOGIN</button>
+                            
+                            <div className="flex justify-center  my-8 ">
+                                <button type="submit" className="py-2 rounded bg-secondary text-white w-10/12 lg:w-full">CREATE YOUR ACCOUNT</button>
 
                             </div>
 
-                            {/* Alternative logins */}
+                            {/* Alternative sign-ins */}
 
                             <div className="flex justify-center mb-6">
                                 <p>or</p>
@@ -60,12 +60,12 @@ return(
 
                         </form>
                     </div>
-                </div> {/* end of logging in portion  */}
+                </div> {/* end of sign up portion  */}
             
             </div> {/* end of container mx-auto */}
         </main>
     </>
 )
 }
-export default Login;
+export default SignUp;
 //,<
--- frontend/src/routes/RoutesComponent.js
+++ frontend/src/routes/RoutesComponent.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import OneMoreStep from "../pages/OneMoreStep";
 import LandingPage from "../pages/LandingPage";
 import FAQs from "../pages/FAQs";
 
@@ -11,6 +13,8 @@ function RoutesComponent() {
         <Route path="/" element={<LandingPage />} />
         <Route path="home" element={<LandingPage />} />
         <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="onemorestep" element={<OneMoreStep />} />
         <Route path="FAQs" element={<FAQs />} />
       </Routes>
     </>
